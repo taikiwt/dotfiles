cb8bd0b6c4fa9b8bef262cf5d1349c0a4036220b
taikiwt <33692664+taikiwt@users.noreply.github.com>
2026-05-30 16:47:59 +0900

    update nvim settings for treesitter & telescope

--- at_config_files/.config/nvim/lua/iatik/plugins/treesitter.lua
+++ at_config_files/.config/nvim/lua/iatik/plugins/treesitter.lua
@@ -1,68 +1,57 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = {
-    "BufReadPre",
-    "BufNewFile",
-  },
-  build = ":TSUpdate",
-  config = function()
-    -- import nvim-treesitter plugin
-    local treesitter = require("nvim-treesitter.configs")
-
-    -- configure treesitter
-    treesitter.setup({ -- enable syntax highlighting
-      -- Lua LS の警告を消すためにデフォルト値を明記
-      sync_install = false,
-      auto_install = true, -- 未インストールのパーサーを自動インストールするかどうか
-      ignore_install = {},
-      modules = {},
-
-      --
-      highlight = {
-        enable = true,
-      },
-      -- enable indentation
-      indent = {
-        enable = true,
-      },
-      -- ensure these language parsers are installed
-      ensure_installed = {
-        "json",
-        "javascript",
-        "typescript",
-        "tsx",
-        "yaml",
-        "toml",
-        "kdl",
-        "html",
-        "css",
-        "scss",
-        "prisma",
-        "markdown",
-        "markdown_inline",
-        "svelte",
-        "graphql",
-        "bash",
-        "lua",
-        "vim",
-        "dockerfile",
-        "gitignore",
-        "query",
-        "vimdoc",
-        "python",
-        "php",
-        "pug",
-        "astro",
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<leader>v",
-          node_incremental = "<leader>v",
-          scope_incremental = false,
-          node_decremental = "<bs>",
+    "nvim-treesitter/nvim-treesitter",
+    event = {
+        "BufReadPre",
+        "BufNewFile",
+    },
+    build = ":TSUpdate",
+    -- main をあえて指定しない、もしくはプラグイン名そのものを指定することで
+    -- Lazy.nvim が内部の新しいエンドポイントを正しく解釈します
+    opts = {
+        ensure_installed = {
+            "json",
+            "javascript",
+            "typescript",
+            "tsx",
+            "yaml",
+            "toml",
+            "kdl",
+            "html",
+            "css",
+            "scss",
+            "prisma",
+            "markdown",
+            "markdown_inline",
+            "svelte",
+            "graphql",
+            "bash",
+            "lua",
+            "vim",
+            "dockerfile",
+            "gitignore",
+            "query",
+            "vimdoc",
+            "python",
+            "php",
+            "pug",
+            "astro",
         },
-      },
-    })
-  end,
+        sync_install = false,
+        auto_install = true,
+        highlight = {
+            enable = true,
+        },
+        indent = {
+            enable = true,
+        },
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = "<leader>v",
+                node_incremental = "<leader>v",
+                scope_incremental = false,
+                node_decremental = "<bs>",
+            },
+        },
+    },
 }
